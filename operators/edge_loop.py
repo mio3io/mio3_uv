ec1e54356e5e07df3c06f3cdbb640d4aba6f25e3
mio3io <2270244+mio3io@users.noreply.github.com>
2024-11-20 15:11:55 +0900

    refactor: adjust edge length

--- operators/edge_loop.py
+++ operators/edge_loop.py
@@ -89,7 +89,7 @@ class MIO3UV_OT_adjust_edge(Mio3UVOperator):
     smooth_factor: FloatProperty(
         name="Smooth",
         description="",
-        default=0.02,
+        default=0.01,
         min=0.0,
         max=1.0,
     )
@@ -112,14 +112,7 @@ class MIO3UV_OT_adjust_edge(Mio3UVOperator):
             node_manager = UVNodeManager(self.objects, mode="EDGE")
 
         for group in node_manager.groups:
-            selected_nodes = list(group.nodes)
-            if selected_nodes:
-                if self.method == "GEOMETRY":
-                    self.mode_geometry(selected_nodes, group.uv_layer)
-                else:
-                    self.mode_evenly(selected_nodes, group.uv_layer)
-
-        for group in node_manager.groups:
+            self.adjust_edges(group)
             group.update_uvs()
 
         node_manager.update_uvmeshes()
@@ -129,9 +122,11 @@ class MIO3UV_OT_adjust_edge(Mio3UVOperator):
         self.print_time(time.time() - self.start_time)
         return {"FINISHED"}
 
-    def mode_geometry(self, nodes, uv_layer):
+    def adjust_edges(self, group):
+        method = self.method
+        nodes = list(group.nodes)
+        node_indices = {node: i for i, node in enumerate(group.nodes)}
         edges = []
-        node_indices = {node: i for i, node in enumerate(nodes)}
 
         for node in nodes:
             for neighbor in node.neighbors:
@@ -142,24 +137,26 @@ class MIO3UV_OT_adjust_edge(Mio3UVOperator):
 
         endpoints = [node for node in nodes if sum(1 for neighbor in node.neighbors if neighbor in nodes) == 1]
 
-        total_3d_length = sum((nodes[edge[0]].vert.co - nodes[edge[1]].vert.co).length for edge in edges)
-        total_uv_length = sum((nodes[edge[0]].uv - nodes[edge[1]].uv).length for edge in edges)
-
-        global_scale_factor = total_uv_length / total_3d_length if total_3d_length > 0 else 1
+        if method == "GEOMETRY":
+            total_3d_length = sum((nodes[edge[0]].vert.co - nodes[edge[1]].vert.co).length for edge in edges)
+            total_uv_length = sum((nodes[edge[0]].uv - nodes[edge[1]].uv).length for edge in edges)
+            scale = total_uv_length / total_3d_length if total_3d_length > 0 else 1
+        else:
+            total_uv_length = sum((nodes[edge[0]].uv - nodes[edge[1]].uv).length for edge in edges)
+            scale = total_uv_length / len(edges) if edges else 0
 
         for _ in range(self.iteration):
             movements = {node: Vector((0, 0)) for node in nodes if node not in endpoints}
 
             for edge in edges:
                 node1, node2 = nodes[edge[0]], nodes[edge[1]]
-                target_length = (node1.vert.co - node2.vert.co).length * global_scale_factor
+                target_length = (node1.vert.co - node2.vert.co).length * scale if method == "GEOMETRY" else scale
 
-                uv1, uv2 = node1.uv, node2.uv
-                uv_edge = uv2 - uv1
+                uv_edge = node2.uv - node1.uv
                 current_length = uv_edge.length
 
                 if current_length > 0:
-                    movement = (target_length - current_length) / 10
+                    movement = (target_length - current_length) / 4 # の割合だけ動かす
                     direction = uv_edge.normalized()
 
                     if node1 not in endpoints:
@@ -167,6 +164,7 @@ class MIO3UV_OT_adjust_edge(Mio3UVOperator):
                     if node2 not in endpoints:
                         movements[node2] += direction * movement
 
+            # スムージング
             smooth_positions = {node: node.uv.copy() for node in nodes if node not in endpoints}
             for node in nodes:
                 if node not in endpoints and node.neighbors:
@@ -188,70 +186,6 @@ class MIO3UV_OT_adjust_edge(Mio3UVOperator):
                 if neighbor not in nodes:
                     relative_pos = neighbor.uv - node.uv
                     neighbor.uv = node.uv + relative_pos
-
-        for node in nodes:
-            node.update_uv(uv_layer)
-
-    def mode_evenly(self, nodes, uv_layer):
-        edges = []
-        node_indices = {node: i for i, node in enumerate(nodes)}
-
-        for node in nodes:
-            for neighbor in node.neighbors:
-                if neighbor in nodes:
-                    edge = tuple(sorted([node_indices[node], node_indices[neighbor]]))
-                    if edge not in edges:
-                        edges.append(edge)
-
-        endpoints = [node for node in nodes if sum(1 for neighbor in node.neighbors if neighbor in nodes) == 1]
-
-        total_uv_length = sum((nodes[edge[0]].uv - nodes[edge[1]].uv).length for edge in edges)
-        num_edges = len(edges)
-
-        target_length = total_uv_length / num_edges if num_edges > 0 else 0
-
-        for _ in range(self.iteration):
-            movements = {node: Vector((0, 0)) for node in nodes if node not in endpoints}
-
-            for edge in edges:
-                node1, node2 = nodes[edge[0]], nodes[edge[1]]
-                uv1, uv2 = node1.uv, node2.uv
-                uv_edge = uv2 - uv1
-                current_length = uv_edge.length
-
-                if current_length > 0:
-                    movement = (target_length - current_length) / 2
-                    direction = uv_edge.normalized()
-
-                    if node1 not in endpoints:
-                        movements[node1] -= direction * movement
-                    if node2 not in endpoints:
-                        movements[node2] += direction * movement
-
-            smooth_positions = {node: node.uv.copy() for node in nodes if node not in endpoints}
-            for node in nodes:
-                if node not in endpoints and node.neighbors:
-                    avg_position = sum((n.uv for n in node.neighbors), Vector((0, 0))) / len(node.neighbors)
-                    smooth_positions[node] = node.uv.lerp(avg_position, self.smooth_factor)
-
-            max_movement = 0
-            for node, movement in movements.items():
-                smooth_movement = smooth_positions[node] - node.uv
-                combined_movement = movement.lerp(smooth_movement, self.smooth_factor)
-                node.uv += combined_movement
-                max_movement = max(max_movement, combined_movement.length)
-
-            if max_movement < 0.00005:
-                break
-
-        for node in nodes:
-            for neighbor in node.neighbors:
-                if neighbor not in nodes:
-                    relative_pos = neighbor.uv - node.uv
-                    neighbor.uv = node.uv + relative_pos
-
-        for node in nodes:
-            node.update_uv(uv_layer)
 
 
 classes = [MIO3UV_OT_circle, MIO3UV_OT_adjust_edge]
