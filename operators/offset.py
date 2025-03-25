3ef31ca01d8bcf5f8988a3b241807efa0972c90c
mio3io <2270244+mio3io@users.noreply.github.com>
2025-03-25 18:25:00 +0900

    update: Change the selection mode

--- operators/offset.py
+++ operators/offset.py
@@ -19,33 +19,25 @@ class MIO3UV_OT_offset(Mio3UVOperator):
     use_mesh_boundary: BoolProperty(name="Mesh Boundary", default=True)
     use_uv_boundary: BoolProperty(name="UV Space Boundary", default=False)
 
-    def invoke(self, context, event):
-        if not context.tool_settings.use_uv_select_sync:
-            bpy.ops.uv.mio3_select_boundary(
-                "INVOKE_DEFAULT",
-                use_mesh_boundary=self.use_mesh_boundary,
-                use_seam=self.use_seam,
-                use_uv_boundary=self.use_uv_boundary,
-            )
-        return self.execute(context)
-
-    def check(self, context):
-        if not context.tool_settings.use_uv_select_sync:
-            bpy.ops.uv.mio3_select_boundary(
-                "INVOKE_DEFAULT",
-                use_mesh_boundary=self.use_mesh_boundary,
-                use_seam=self.use_seam,
-                use_uv_boundary=self.use_uv_boundary,
-            )
-        return True
-
     def execute(self, context):
         self.start_time()
         self.objects = self.get_selected_objects(context)
 
+        if context.tool_settings.uv_select_mode in {"FACE", "ISLAND"}:
+            context.tool_settings.uv_select_mode = "VERTEX"
+
+        if not context.tool_settings.use_uv_select_sync:
+            bpy.ops.uv.mio3_select_boundary(
+                "INVOKE_DEFAULT",
+                use_mesh_boundary=self.use_mesh_boundary,
+                use_seam=self.use_seam,
+                use_uv_boundary=self.use_uv_boundary,
+            )
+
         use_uv_select_sync = context.tool_settings.use_uv_select_sync
         if use_uv_select_sync:
             self.sync_uv_from_mesh(context, self.objects)
+            context.tool_settings.mesh_select_mode = (True, False, False)
 
         island_manager = UVIslandManager(self.objects)
 
@@ -60,7 +52,6 @@ class MIO3UV_OT_offset(Mio3UVOperator):
     def expand_uv_boundary_outward(self, island, offset):
         uv_layer = island.uv_layer
 
-        # island_verts = set(vert for face in island.faces for vert in face.verts)
         selected_uv_edges = []
         for face in island.faces:
             if face.select:
